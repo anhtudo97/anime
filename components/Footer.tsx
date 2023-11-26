8e59c4e08edfdd21b2f6dcee526c881a35ee3fc0
anhtudo97 <anhtudo97@gmail.com>
2023-11-26 22:39:45 +0700

    feat: components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,6 +10,7 @@ function Hero() {
                     width={101}
                     height={96}
                     className="object-contain"
+                    sizes="(max-width: 24px) 100vw, (max-width: 48px) 50vw, 33vw"
                 />
                 <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
                     Explore The <span className="red-gradient">Diverse Realms</span> of
@@ -17,7 +18,7 @@ function Hero() {
                 </h1>
             </div>
             <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
-                <Image src="/anime.png" alt="anime" fill className="object-contain" />
+                <Image src="/anime.png" alt="anime" fill className="object-contain" sizes="(max-width: 600px) 100vw, (max-width: 100px) 50vw, 33vw" />
             </div>
         </header>
     );
